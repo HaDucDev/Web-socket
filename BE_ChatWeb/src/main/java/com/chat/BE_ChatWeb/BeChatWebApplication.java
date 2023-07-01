949dd04a0d6c7b66e3ad29eb265a55423ef89f93
HaDucDev <haductrung392000@gmail.com>
2023-07-01 22:00:41 +0700

    start

---
+++ BE_ChatWeb/src/main/java/com/chat/BE_ChatWeb/BeChatWebApplication.java
@@ -0,0 +1,14 @@
+package com.chat.BE_ChatWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeChatWebApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ok hay qua nhi");
+		SpringApplication.run(BeChatWebApplication.class, args);
+	}
+
+}
---
+++ BE_ChatWeb/src/test/java/com/chat/BE_ChatWeb/BeChatWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.BE_ChatWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeChatWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
